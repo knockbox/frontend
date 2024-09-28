943fcd12abb8e4b24a07be2cfcf20d90d3f65191
Christopher Oswald <christopher@heckin.dev>
2024-09-27 18:38:02 -0700

    add index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {IndexComponent} from "./index/index.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: "Index",
+    path: "",
+    pathMatch: "prefix",
+    component: IndexComponent
+  }
+];
