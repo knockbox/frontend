419e0855d238e1a429c2d0b108fa1f0c8fb0c204
Christopher Oswald <christopher@heckin.dev>
2024-09-27 18:53:12 -0700

    add environments

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment} from "../../environments/environment";
 
 @Component({
-  selector: 'app-index',
-  standalone: true,
-  imports: [],
-  templateUrl: './index.component.html',
-  styleUrl: './index.component.scss'
+    selector: 'app-index',
+    standalone: true,
+    imports: [],
+    templateUrl: './index.component.html',
+    styleUrl: './index.component.scss'
 })
 export class IndexComponent {
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  authApiUrl: "http://localhost:9877/api",
+  matchboxApiUrl: "http://localhost:9090/api"
+};
